429d84ac5c07798f93803fb5dd8a868b74537d95
pritapuja <prita.pujaastuti@gmail.com>
2024-10-12 14:03:21 +0700

    feat: initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ test('renders App component with NavBar, image banner, and Footer', async () => 
   render(<App />);
 
   // image banner is rendered 
-  const banner = screen.getByTestId('image-banner')
-  expect(banner).toBeInTheDocument();
-  expect(banner).toHaveProperty('src', 'change-to-link-url')
+  // const banner = screen.getByTestId('image-banner')
+  // expect(banner).toBeInTheDocument();
+  // expect(banner).toHaveProperty('src', 'change-to-link-url')
 });
--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Card from './Card';
 
@@ -11,7 +11,28 @@ const el = {
   id: 1
 };
 
-test('renders the component Card & given props', () => {
+test('renders the component Card with given props', () => {
   render(<Card el={el} />);
 
+  const image = screen.getByTestId('img-recipe-1');
+  expect(image).toHaveAttribute('src', el.image);
+  expect(image).toHaveAttribute('alt', el.name);
+
+  const title = screen.getByTestId('title-recipe-1');
+  expect(title).toHaveTextContent(el.name);
+
+  const rating = screen.getByTestId('rating-recipe-1');
+  expect(rating).toHaveTextContent(el.rating.toString());
+
+  const tag1 = screen.getByTestId('tag-recipe-Tag1');
+  const tag2 = screen.getByTestId('tag-recipe-Tag2');
+  expect(tag1).toHaveTextContent('Tag1');
+  expect(tag2).toHaveTextContent('Tag2');
+  
+  const tag3 = screen.queryByTestId('tag-recipe-Tag3');
+  expect(tag3).not.toBeInTheDocument();
+
+  const link = screen.getByTestId('link-recipe-1');
+  expect(link).toHaveAttribute('href', 'https://dummyjson.com/recipes/1');
+  expect(link).toHaveTextContent('READ MORE');
 });
