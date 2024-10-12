86134920752c30fa7e9fa1014185dd0338db824e
pritapuja <prita.pujaastuti@gmail.com>
2024-10-12 16:15:22 +0700

    feat: initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,68 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders App component with NavBar, image banner, and Footer', async () => {
-  render(<App />);
+const mockRecipes = {
+  recipes: [
+    {
+      id: 1,
+      name: 'Classic Margherita Pizza',
+      tags: ["Pizza", "Italian"],
+      image: "https://cdn.dummyjson.com/recipe-images/1.webp",
+      rating: 4.6,
+    },
+    {
+      id: 2,
+      name: 'Spaghetti Carbonara',
+      tags: ["Pasta", "Italian"],
+      image: "https://cdn.dummyjson.com/recipe-images/2.webp",
+      rating: 4.5,
+    },
+  ],
+};
 
-  // image banner is rendered 
-  // const banner = screen.getByTestId('image-banner')
-  // expect(banner).toBeInTheDocument();
-  // expect(banner).toHaveProperty('src', 'change-to-link-url')
+beforeEach(() => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(mockRecipes),
+    })
+  );
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+test('fetches recipes', async () => {
+  render(<App />);
+  expect(screen.getByTestId('my-recipe')).toBeInTheDocument();
+  
+  const bannerImage = screen.getByTestId('image-banner');
+  expect(bannerImage).toBeInTheDocument();
+  expect(bannerImage).toHaveAttribute('src', 'https://www.instacart.com/company/wp-content/uploads/2022/11/cooking-statistics-hero.jpg');
+
+  await waitFor(() => {
+    expect(screen.getByText('Classic Margherita Pizza')).toBeInTheDocument();
+
+  });
+});
+
+test('search function', async () => {
+  render(<App />);
+  
+  await waitFor(() => {
+    expect(screen.getByText('Classic Margherita Pizza')).toBeInTheDocument();
+
+  });
+  
+  const searchInput = screen.getByPlaceholderText('Recipe Name');
+  fireEvent.change(searchInput, { target: { value: 'Classic Margherita Pizza' } });
+
+  const searchForm = screen.getByTestId('form-search');
+  fireEvent.submit(searchForm);
+  
+  await waitFor(() => {
+    expect(screen.getByText('Classic Margherita Pizza')).toBeInTheDocument();
+  });
 });
